8eaf8f01cb0d97b8b17b1c04c703fe599fe165df
Michael Bestas <mkbestas@lineageos.org>
2025-04-25 03:10:20 +0300

    thyme: Migrate powershare to soong_config_set
    Change-Id: I2489e8d95d968556a854d7b8314f098d37c382b7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,9 +25,6 @@ TARGET_KERNEL_CONFIG += vendor/xiaomi/thyme.config
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := thyme
 
-# PowerShare
-TARGET_POWERSHARE_PATH := /sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-02/c440000.qcom,spmi:qcom,pm8150b@2:qcom,qpnp-smb5/power_supply/wireless/reverse_chg_mode
-
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
