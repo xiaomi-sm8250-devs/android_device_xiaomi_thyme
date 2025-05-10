571105d8f2947cd417038245dec4f98d73dcaced
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-10 18:12:41 +0800

    thyme: Drop `TARGET_RECOVERY_DEVICE_MODULES`
    * It's deprecated on 15 QPR2
    * Building a library as module doesn't get it executed? and libinit
      still does its job in recovery mode without migrating this
    
    Change-Id: I7be000356fe7d7bd099615d334892d1686bece0e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,6 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_thyme
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_thyme
 
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/xiaomi/thyme.config
